2a0ce18d93a128169c94d934776927eb7b3f7c17
Alexander Gatsenko <alexandr.gatsenko@gmail.com>
2018-09-17 04:13:20 +0300

    make progress for LocalFsStorage

---
+++ modules/core/mylib-core-infrastructure/src/test/scala/io/agatsenko/mylib/infrastructure/filestorage/localfs/LocalFsStorageTests.scala
@@ -0,0 +1,12 @@
+/**
+  * Author: Alexander Gatsenko (alexandr.gatsenko@gmail.com)
+  * Created: 2018-09-17
+  */
+package io.agatsenko.mylib.infrastructure.filestorage.localfs
+
+import org.scalatest.{FunSuite, Matchers}
+
+class LocalFsStorageTests extends FunSuite with Matchers {
+  // FIXME: not yet implemented
+  ???
+}
